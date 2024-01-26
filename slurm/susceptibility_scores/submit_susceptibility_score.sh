10d288a9587b260b790ba2e345e5f5ebd3ac9788
Kevin Du <kevin.du20@gmail.com>
2024-01-26 03:13:01 +0100

    Add entity_uri_to_predicate_degree_path.json and minor cleanup of yago_generate_qec.ipynb

--- slurm/susceptibility_scores/submit_susceptibility_score.sh
+++ slurm/susceptibility_scores/submit_susceptibility_score.sh
@@ -3,5 +3,5 @@
 echo ${which python};
 echo $PWD;
 echo "previewing args";
-echo $1 -P $2 -S $3 -M $4 -Q $5 -MC $6 -ME $7 -ET $8 -QT $9 -AM ${10} ${11} ${12} ${13} ${14} ${15} ${16};
+echo $1 -P $2 -S $3 -M $4 -Q $5 -MC $6 -ME $7 -ET $8 -QT $9 -AM ${10} -ES ${11} ${12} ${13} ${14} ${15} ${16};
 python susceptibility_scores.py $1 -P $2 -S $3 -M $4 -Q $5 -MC $6 -ME $7 -ET $8 -QT $9 -AM ${10} -ES ${11} ${12} ${13} ${14} ${15} ${16}; # last 5 are boolean flags
